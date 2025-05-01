796ba20a6ef1df75b734a8330c2e60676d95b75a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-01 01:15:55 -0300

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Lista todos os yml e baixa cada um
+ls *.yml | while read yml; do
+  echo "Removendo projeto: $yml"
+  docker-compose -f $yml stop && docker-compose -f $yml rm -f
+done
+echo ""
+docker volume prune -f && git pull
+
+echo ""
+echo "Removendo volumes:"
+echo ""
+# Lista todos os volumes e remove cada um
+docker volume ls -q | grep relatoriosbi | while read volume; do
+  #echo "Removendo volume: $volume"
+  docker volume rm "$volume"
+done
+echo ""
