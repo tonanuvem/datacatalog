e7b325b9acdec482a459b6c32809db92c7bd2763
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-21 15:38:41 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,27 +23,7 @@ docker-compose -f docker-compose-jupyter.yml up -d
 
 
 ### OPEN METADATA PARA DEMONSTRAR GOVERNANÇA DE DADOS, ESPECIALMENTE DATA DISCOVERY E DATA QUALITY
-
-curl -sL -o docker-compose-metadata.yml https://github.com/open-metadata/OpenMetadata/releases/download/1.7.0-release/docker-compose.yml
-
-# comentar (adicionar # no início) da linha que começa com "version:" no arquivo YAML:
-sed -i '/^version:/ s/^/# /'  docker-compose-metadata.yml
-
-docker-compose -f docker-compose-metadata.yml up -d
-
-AIRFLOW_msglog="INFO - Starting the scheduler"
-OPENMETADATA_msglog="Started application"
-
-echo ""
-echo "Aguardando a configuração do OPEN METADATA."
-while [ "$(docker logs openmetadata_ingestion 2>&1 | grep "$AIRFLOW_msglog" | wc -l)" != "1" ]; do
-  printf "."
-  sleep 1
-done
-while [ "$(docker logs openmetadata_server 2>&1 | grep "$OPENMETADATA_msglog" | wc -l)" != "1" ]; do
-  printf "."
-  sleep 1
-done
+sh local_run_openmetadata.sh
 
 #echo ""
 #echo "Configurando o CDC"
