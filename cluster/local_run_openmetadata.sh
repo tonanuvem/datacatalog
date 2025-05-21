625067af5adec9c626fe08fd5602f2de3ae67e96
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-21 14:31:53 -0300

    Update local_run_openmetadata.sh

--- cluster/local_run_openmetadata.sh
+++ cluster/local_run_openmetadata.sh
@@ -2,7 +2,7 @@
 
 #curl -sL -o docker-compose-metadata.yml https://github.com/open-metadata/OpenMetadata/releases/download/1.7.0-release/docker-compose.yml
 
-docker-compose -f docker-local-metadata.yml up -d
+docker-compose -f docker-local-openmetadata.yml up -d
 
 AIRFLOW_msglog="INFO - Starting the scheduler"
 OPENMETADATA_msglog="Started application"
