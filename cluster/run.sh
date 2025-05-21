6d5aac9fcd9061aeeabd24adc369345cb16a02c9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-21 14:20:45 -0300

    Update run.sh

--- cluster/run.sh
+++ cluster/run.sh
@@ -42,23 +42,11 @@ docker stack deploy --compose-file docker-compose-jupyter.yml dataops
 
 ### OPEN METADATA:
 
-docker stack deploy --compose-file docker-compose-openmetadata.yml dataops
-#curl -sL -o docker-compose-metadata.yml https://github.com/open-metadata/OpenMetadata/releases/download/1.7.0-release/docker-compose.yml
-#docker-compose -f docker-compose-metadata.yml up -d
-
-AIRFLOW_msglog="INFO - Starting the scheduler"
-OPENMETADATA_msglog="Started application"
-
 echo ""
-echo "Aguardando a configuração do OPEN METADATA."
-while [ "$(docker logs openmetadata_ingestion 2>&1 | grep "$AIRFLOW_msglog" | wc -l)" != "1" ]; do
-  printf "."
-  sleep 1
-done
-while [ "$(docker logs openmetadata_server 2>&1 | grep "$OPENMETADATA_msglog" | wc -l)" != "1" ]; do
-  printf "."
-  sleep 1
-done
+echo "Rodando local o OPEN METADATA"
+echo ""
+sh run_local_openmetadata.sh
+
 
 echo ""
 echo "Configurando o CDC"
